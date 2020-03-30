e2829a9d7825c395be89fbf9a83f48fa2f5fca94
Sanidhya Anand <62481129+sanidhyaanand@users.noreply.github.com>
2020-03-30 21:59:02 +0530

    nice

---
+++ OpenGL-Code/pacman_x.cpp
@@ -0,0 +1,305 @@
+// SoC Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 600;
+const unsigned int SCR_HEIGHT = 600;
+const GLchar* vertexShaderSource = "#version 410 core\n"
+"layout ( location = 0 ) in vec3 position;\n"
+"layout (location = 0) in vec2 aTexCoord;\n"
+"out vec3 rgb;\n"
+"out vec2 TexCoord;\n"
+"void main ( )\n"
+"{\n"
+"gl_Position = vec4( position.x, position.y, position.z, 1.0 );\n"
+"rgb = vec3(1.0,1.0,0.0);\n"
+"TexCoord=aTexCoord;\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 410 core\n"
+"in vec3 rgb;\n"
+"out vec4 color;\n"
+"void main ( )\n "
+"{\n"
+"color = vec4 (rgb, 1.0);\n"
+"}\0";
+const GLchar* fragmentShaderSourcexx = "#version 410 core\n"
+"in vec2 TexCoord;\n"
+"out vec4 FragColor;\n"
+"uniform sampler2D ourTexture;\n"
+"void main()\n"
+"{\n"
+"FragColor=texture(ourTexture,TexCoord);\n"
+"}\0";
+
+const GLchar* fragmentShaderSourcex = "#version 410 core\n"
+"out vec4 color;\n"
+"uniform vec4 changeColor;\n"
+"void main()\n"
+"{\n"
+"color = changeColor;\n"
+"}\0";
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Allahu Akbar", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    int fragmentShaderx = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderx, 1, &fragmentShaderSourcex, NULL);
+    glCompileShader(fragmentShaderx);
+
+    int fragmentShaderxx = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderxx, 1, &fragmentShaderSourcexx, NULL);
+    glCompileShader(fragmentShaderxx);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShaderxx, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShaderxx, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);        //simply make this fragmentshaderx
+    glLinkProgram(shaderProgram);                          //for time varying color 
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    int shaderProgramx = glCreateProgram();
+    glAttachShader(shaderProgramx, vertexShader);
+    glAttachShader(shaderProgramx, fragmentShaderx);
+    glLinkProgram(shaderProgramx);
+    // check for linking errors
+    glGetProgramiv(shaderProgramx, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgramx, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShaderx);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    GLfloat vertices1[6000], m=0;
+    vertices1[0] = 0.0;
+    vertices1[1] = 0.0;
+    vertices1[2] = 0.0;
+         for (int i = 3; i < 3 * 1025 ; i=i+3)
+         {
+            vertices1[i] =0.5 * cos((m/1024.0) * 2 * 3.15);
+            vertices1[i + 1] = 0.5 * sin((m/1024.0) * 2 * 3.15);
+            vertices1[i + 2] = 0.0;
+            m++;
+          //std::cout << vertices1[i] << " " << vertices1[i + 1] << "\n";
+         }
+        
+    
+    unsigned int indices1[4000], k=128;
+    for (int j = 0; j < 3*1028-3*256; j = j + 3)
+        {
+        indices1[j] = 0;
+        indices1[j + 1] = k + 1;
+        indices1[j + 2] = k + 2;
+        k++;
+        }
+    
+
+    GLuint VBO[2], VAO[2], EBO[2];
+    glGenVertexArrays(2, VAO);
+    glGenBuffers(2, VBO);
+    glGenBuffers(2, EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices1), indices1, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,  * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    
+
+    
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+     
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("C:/Users/Sanidhya/Desktop/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        
+        
+         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        
+
+       
+        float time = glfwGetTime();
+        float green = sin(time) / 2.0 + 0.5;
+        float red = cos(time) / 2.0 + 0.5;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "changeColor");
+        glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, red, green, 0.0, 1.0);
+        glBindVertexArray(VAO[0]);
+        glDrawElements(GL_TRIANGLES, sizeof(indices1), GL_UNSIGNED_INT, 0);
+       
+        glBindVertexArray(0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO[2]);
+    glDeleteBuffers(1, &VBO[2]);
+    glDeleteBuffers(1, &EBO[2]);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ README-CODE.md.txt
@@ -0,0 +1,19 @@
+Hi!
+
+Sorry for not making an MD file, I don't have a reader for it. I'll figure it out.
+
+You should find all the code in the OpenGL-Code folder, and all textures used
+in the texture folder. 
+
+There should be three fragment shaders for all the codes using textures:
+1. A normal gradient based shader, or maybe just plain color.(fragmentshader)
+2. A time varying shader.(fragmentshaderx)
+3. A texture.(fragmentshaderxx)
+
+I know I should've defined a class for the shaders, but as of Monday 9PM, I
+still haven't coded the whole class structure once by myself; It will surely be
+present in further projects. A lot of time was spent in figuring out Arrays :P
+
+Feel free to use all the textures. There are some circular ones specifically for
+the disc, but somehow it was just not working and i reverted back to a simpler
+one.  
